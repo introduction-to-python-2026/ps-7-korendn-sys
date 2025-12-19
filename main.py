bc16f8a70a334e6dfc7ced9f4243d4f4ceb52bd2
korendn-sys <korendn@gmail.com>
2025-12-19 13:37:31 +0200

    Add correlation plot for Iris dataset

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+
+x = df["sepal length (cm)"]
+y = df["petal length (cm)"]
+
+sns.regplot(x=x, y=y)
+plt.title("Correlation: Sepal Length vs Petal Length")
+plt.savefig("correlation.png")
+plt.show()
 
